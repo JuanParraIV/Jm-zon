2b0440ef6c301ed5882b6825936f852a5f40d70e
JuanParraIV <jmparra1993@gmail.com>
2022-06-07 19:39:55 -0500

    banner component finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Jm-zon</title>
       </Head>
-      
-      <Header/>
+
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+      {/* BANNER */}
+        <Banner/>
+
+
+      </main>
+
     </div>
   );
 }
