d55ff41935395011261fd92f4b4f2794c48a3bd5
JuanParraIV <jmparra1993@gmail.com>
2022-06-08 10:40:55 -0500

    Finish product feed component & product component fully responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
+
   return (
     <div className="bg-gray-100">
       <Head>
@@ -12,12 +15,23 @@ export default function Home() {
       <Header />
 
       <main className="max-w-screen-2xl mx-auto">
-      {/* BANNER */}
-        <Banner/>
+        {/* BANNER */}
+        <Banner />
 
-
+        <ProductFeed products={products}/>
       </main>
-
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    }
+  }
+};
