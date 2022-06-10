5b5258364083fa85b3c295f577c0dcf6a3d541cd
JuanParraIV <jmparra1993@gmail.com>
2022-06-09 19:23:03 -0500

    redux toolkit implemented

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,9 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import React, { useState } from "react";
-import CurrencyFormat from 'react-currency-format';
+import CurrencyFormat from "react-currency-format";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const Product = ({
   id,
@@ -12,7 +14,8 @@ const Product = ({
   image,
   rating,
 }) => {
-    const [rate, setQuantity] = useState(rating.rate);
+  const dispatch = useDispatch();
+  const [rate, setQuantity] = useState(rating.rate);
   const ratingStar = (rating) => {
     let star = rating.rate;
     let stars = [];
@@ -21,11 +24,27 @@ const Product = ({
     }
     return stars;
   };
+  const addItemToBasket = () => {
+    const product={
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      rating,
+    }
+
+    //SENDING The product("payload")as an action to the redux store -- the basket slice
+    dispatch(addToBasket(product));
+  };
 
   const [hasPrime, setHasPrime] = useState(Math.random() < 0.5);
   return (
-    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-      <p className='absolute top-2 right-2 text-xs italic text-gray-500'>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-500">
+        {category}
+      </p>
       <Image
         src={image}
         width={200}
@@ -34,26 +53,30 @@ const Product = ({
         alt="Product"
         className=""
       />
-      <h4 className='my-3 '>{title}</h4>
-      <div className="flex">{ratingStar(rating)} <p className='text-xs text-gray-500'> {rate}</p></div>
-      <p className='text-xs my-2 line-clamp-2'>{description}</p>
+      <h4 className="my-3 ">{title}</h4>
+      <div className="flex">
+        {ratingStar(rating)} <p className="text-xs text-gray-500"> {rate}</p>
+      </div>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-      <div className='mb-5'>
+      <div className="mb-5">
         <CurrencyFormat
-            value={price}
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
+          value={price}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"$"}
         />
       </div>
-      {hasPrime &&(
-          <div className='flex items-center space-x-2 -mt-5'>
-              <img className='w-12' src="https://links.papareact.com/fdw" alt="" />
-              <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
-          </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        </div>
       )}
 
-        <button className='mt-auto button'> Add to the Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to the Basket
+      </button>
     </div>
   );
 };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Header from '../components/Header'
+import { selectItems } from '../slices/basketSlice'
 
 const Checkout = () => {
+  const items =useSelector(selectItems);
+  console.log(items);
   return (
     <div className='bg-gray-100'>
     <Header/>
@@ -19,7 +23,13 @@ const Checkout = () => {
             objectFit='contain'
           />
           <div className='flex flex-col p-5 space-y-10 bg-white'>
-              <h1 className='text-3xl border-b pb-4 '>Your shopping cart </h1>
+              <h1 className='text-3xl border-b pb-4 '>
+                {items.length===0 ? 'Your Amazon basket is empty':'Your Shopping basket'}
+              </h1>
+
+              {items && items?.map(()=>(
+                  
+              ))}
           </div>
       </div>
 
